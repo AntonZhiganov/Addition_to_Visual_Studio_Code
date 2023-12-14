d6d819b86c7155bfedc03ad9fbd6294f6f1ecd65
AntonZhiganov <Antohajigan@gmail.com>
2023-12-14 19:15:07 +0100

    Now if we enter folder/file in the field, then a folder is created in our folder in which the file is created

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 import pygame,sys
+import sys
+import os
 
 pygame.init()
 
-clock = pygame.time.Clock()
+clock = pygame.time.Clock()        #Controlling the speed of updates
 screen = pygame.display.set_mode((900,75))  #Size input screen
 
 pygame.display.set_caption('text input')      #Name input screen
 
+#User text, input field, background, background color, input field color
+
 userText = ' '
-font = pygame.font.SysFont('frenchscript',32) 
+font = pygame.font.SysFont('Arial',32) 
 input_rect = pygame.Rect(10,15,500,40)
 active = False
 colorAktiv = pygame.Color('green')
@@ -18,22 +22,40 @@ active = False
 
 while True:    
     
-   for event in pygame.event.get():
+   for event in pygame.event.get():   #Checking for program exit events
       if event.type==pygame.QUIT:
          pygame.quit()
          sys.exit()
               
-      if event.type==pygame.MOUSEBUTTONDOWN:
+      if event.type==pygame.MOUSEBUTTONDOWN:         #Activate a field by clicking
          if input_rect.collidepoint(event.pos):
             active = True
 
-      if event.type == pygame.KEYDOWN:
+      if event.type == pygame.KEYDOWN:       #If button press
          if active == True:
          
-            if event.key == pygame.K_BACKSPACE:
+            if event.key == pygame.K_BACKSPACE:     #If K_BACKSPACE press - erase last character
                userText = userText[:-1]
+               
+            elif event.key == pygame.K_RETURN:      #If K_RETURN press - create folder/file
+               print("User input:", userText)
+               
+               try:
+                  folder, filename = userText.split('/')
+                  os.makedirs(folder, exist_ok=True)
+                  
+                  with open(userText, 'w') as file:
+                     file.write("Your initial content here.")
+                     print(f"Folder '{folder}' and file '{filename}' created successfully.")
+                     
+               except ValueError:
+                  print("Invalid input format. Please use 'folder/filename'.")
+                  
+               except Exception as e:
+                  print(f"An error occurred: {e}")
+                  
             else:
-               userText+=event.unicode
+               userText+=event.unicode              #Add char in string
           
    screen.fill('black')
    if active:
